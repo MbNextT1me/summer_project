e6040a02a504badb318e77ddbd3541f68652dab0
Svyatoslav <sviatoslavnazin@gmail.com>
2021-07-04 10:36:40 +0800

    Add an adaptive web design for all resolutions
    Media resolutions: 1750, 1175, 992, 768, 480.

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,26 @@ import documentReady from './helpers/documentReady';
 documentReady(() => {
 	lazyImages();
 });
+
+document.addEventListener('scroll', () => {
+	const header = document.querySelector('.header');
+
+	if (window.pageYOffset > 1) {
+		header.classList.add('header-scroll');
+	} else {
+		header.classList.remove('header-scroll');
+	}
+});
+
+document.addEventListener('click', (event) => {
+	const flag = event.target.classList.contains('hamburger');
+
+	if (!flag) return;
+
+	const nav = document.querySelector('.nav');
+	const hamburger = event.target;
+
+	hamburger.classList.toggle('hamburger-open');
+	nav.classList.toggle('nav-active');
+	document.body.classList.toggle('page-lock');
+});
